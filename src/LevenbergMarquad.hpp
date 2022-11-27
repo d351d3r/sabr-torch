3117c038a867afa9454c3b3114c2f6b42bb2e36c
Anatolii Cherednichenko <greatestcombinator@gmail.com>
2022-11-27 21:11:13 +1000

    update

--- src/LevenbergMarquad.hpp
+++ src/LevenbergMarquad.hpp
@@ -5,81 +5,100 @@
 
 template <typename T>
 int length(T n) {
-    int l = 0;
-    do {
-        l++;
-        n /= 10;
-    } while(n);
-    return l;
+  int l = 0;
+  do {
+    l++;
+    n /= 10;
+  } while (n);
+  return l;
 }
 
 class LevenbergMarquad {
-public:
-    LevenbergMarquad(torch::Tensor& init_p,torch::Tensor& x, torch::Tensor& y, 
-    torch::Tensor& func,torch::Tensor eps1,torch::Tensor eps2,torch::Tensor eps3,torch::Tensor eps4,
-    torch::Tensor lm_up,torch::Tensor lm_down) {
+ public:
+  LevenbergMarquad(
+      torch::Tensor& init_p,
+      torch::Tensor& x,
+      torch::Tensor& y,
+      torch::Tensor& func,
+      float sigma,
+      int lambda_lm,
+      float eps1,
+      float eps2,
+      float eps3,
+      float eps4,
+      float lm_up,
+      float lm_down) {
+    unsigned int iter_n = 0;
 
-        unsigned int iter_n = 0;
+    // https://pytorch.org/cppdocs/api/function_namespaceat_1a095f3dd9bd82e1754ad607466e32d8e2.html?highlight=detach#_CPPv4N2at11detach_copyERKN2at6TensorE
+    // https://pytorch.org/cppdocs/api/classat_1_1_tensor.html?highlight=requires_grad_#_CPPv4NK2at6Tensor14requires_grad_Eb
 
-        // https://pytorch.org/cppdocs/api/function_namespaceat_1a095f3dd9bd82e1754ad607466e32d8e2.html?highlight=detach#_CPPv4N2at11detach_copyERKN2at6TensorE
-        // https://pytorch.org/cppdocs/api/classat_1_1_tensor.html?highlight=requires_grad_#_CPPv4NK2at6Tensor14requires_grad_Eb
-
-        p = torch::detach_copy(init_p);
-        torch::requires_grad(true);
-        {
-            torch::NoGradGuard no_grad;
-            auto J = torch::autograd::grad(func, p);
-            const torch::autograd::variable_list J = torch::autograd::grad(func, p);
-        }
-        auto W = torch::diag(static_cast<torch::Tensor>((1 / pow(sigma, 2)) * length(x)));
+    p = torch::detach_copy(init_p);
+    torch::requires_grad(true);
+    {
+      torch::NoGradGuard no_grad;
+      // TODO hardcode
+    //   auto J = torch::autograd::grad(func, p);
+    //   const torch::autograd::variable_list J = torch::autograd::grad(func, p);
+    
     }
+    auto W = torch::diag(((1 / pow(sigma, 2)) * length(x)));
+  }
 
-// https://pytorch.org/cppdocs/api/typedef_namespacetorch_1abf2c764801b507b6a105664a2406a410.html?highlight=torch%20no_grad
-    void broyden_jacobian_update(torch::Tensor J,torch::Tensor dp) {
-        torch::NoGradGuard no_grad;
+  // https://pytorch.org/cppdocs/api/typedef_namespacetorch_1abf2c764801b507b6a105664a2406a410.html?highlight=torch%20no_grad
+  void broyden_jacobian_update(torch::Tensor J, torch::Tensor dp) {
+    torch::NoGradGuard no_grad;
 
-        torch::Tensor df = func(p + dp) - func(p);
-        J += torch::outer(df - torch::mv(J, dp),dp)::div(torch::linalg::norm(dp, ord = 2));
-    }
+    torch::Tensor df = func(p + dp) - func(p);
+    J += torch::outer(
+        df - torch::mv(J, dp), dp)::div(torch::linalg::norm(dp, ord = 2));
+  }
 
-    void torch_jacobian_update(torch::Tensor &p) {
-        //         Finite-difference Jacobian update
-//        torch::NoGradGuard no_grad;
-//        J = torch::autograd::functional::jacobian(func, p);
-        // TODO
-        return;
+  void torch_jacobian_update(torch::Tensor& p) {
+    //         Finite-difference Jacobian update
+    //        torch::NoGradGuard no_grad;
+    //        J = torch::autograd::functional::jacobian(func, p);
+    // TODO
+    return;
+  }
 
-    }
+  void solve_for_dp() {
+    // Solver for optimizer step
+    torch::NoGradGuard no_grad;
+    static torch::Tensor JTW =
+        torch::matmul(torch::transpose(J, 0, 1), this->W);
+    static torch::Tensor JTWJ = torch::matmul(JTW, J);
 
-    void solve_for_dp() {
-        // Solver for optimizer step
-        torch::NoGradGuard no_grad;
-        static torch::Tensor JTW = torch::matmul(torch::transpose(J, 0, 1), this->W);
-        static torch::Tensor JTWJ = torch::matmul(JTW, J);
+    torch::Tensor dy = y - func(p);
+    torch::Tensor dp = torch::linalg::solve(
+        JTWJ + lambda_lm * torch::diag(torch::diagonal(JTWJ)),
+        torch::mv(JTW, dy));
+  }
 
-        torch::Tensor dy = y - func(p);
-        torch::Tensor dp = torch::linalg::solve(JTWJ + lambda_lm * torch::diag(torch::diagonal(JTWJ)), torch::mv(JTW, dy));
-    }
+  void chi_2(torch::Tensor p, torch::Tensor& W, torch::Tensor& y) {
+    torch::NoGradGuard no_grad;
+    //
+    //        chi2 = y^T.W.y + 2 * y^T.W . y_hat +  (y-hat)^T.W.y_hat
+    //
+    auto y_hat = func(p);
 
-
-    void chi_2(torch::Tensor p,torch::Tensor& W,torch::Tensor& y) {
-        torch::NoGradGuard no_grad;
-//
-//        chi2 = y^T.W.y + 2 * y^T.W . y_hat +  (y-hat)^T.W.y_hat
-//
-        auto y_hat = func(p);
-
-        return (torch::dot(y, torch::mv(W, y)) - 2
-        * torch::dot(y, torch::mv(W, y_hat)) +
+    return (
+        torch::dot(y, torch::mv(W, y)) -
+        2 * torch::dot(y, torch::mv(W, y_hat)) +
         torch::dot(y_hat, torch::mv(W, y_hat)));
-    }
+  }
 
-    bool rho(torch::Tensor W,torch::Tensor y,torch::Tensor dp, torch::Tensor JTWJ) {
-        torch::NoGradGuard no_grad;
+  bool rho(
+      torch::Tensor W,
+      torch::Tensor y,
+      torch::Tensor dp,
+      torch::Tensor JTWJ) {
+    torch::NoGradGuard no_grad;
 
-//    rho =  chi2(p) - chi2(p + dp) / (dp)^T . ( lambda * diag(J^T W J).dp + J^T W . dy )
+    //    rho =  chi2(p) - chi2(p + dp) / (dp)^T . ( lambda * diag(J^T W J).dp +
+    //    J^T W . dy )
 
-        dy = y - func(p);
+    dy = y - func(p);
         float i_rho = ((chi_2(p,W,y) - chi_2(p + dp,W,y).div(torch::dot(dp, torch::mv(lambda_lm
                                                                                 * torch::diag(torch::diagonal(JTWJ)),
                                                                                 dp) + torch::mv(JTW, dy))));
@@ -88,51 +107,47 @@ public:
             return true;
         else
             return false;
-    }
+  }
 
-    void update_p(const torch::Tensor& dp, torch::Tensor p) {
+  void update_p(const torch::Tensor& dp, torch::Tensor p) {
         torch::NoGradGuard no_grad;
         p += dp;
-    }
+  }
 
-    void step() {
-
+  void step() {
         solve_for_dp();
 
         if (rho()) {
-            update_p(dp);
-            lambda_lm = torch::maximum(lambda_lm / lm_down, torch::Tensor(1e-7));
+          update_p(dp);
+          lambda_lm = torch::maximum(lambda_lm / lm_down, torch::Tensor(1e-7));
         } else
-            lambda_lm = torch::minimum(lambda_lm * lm_up, torch::Tensor(1e7));
+          lambda_lm = torch::minimum(lambda_lm * lm_up, torch::Tensor(1e7));
 
         if (iter_n % (2 * len(p)) == 0)
-            broyden_jacobian_update(J,dp);
+          broyden_jacobian_update(J, dp);
         else {
-            p.requires_grad_(true);
-            torch_jacobian_update(p);
-            iter_n++;
-            return p;
+          p.requires_grad_(true);
+          torch_jacobian_update(p);
+          iter_n++;
+          return p;
         }
+        void condition1() {}
+        //    Add condition for Broyden 1-rank method convergence
+  }
 
-        void condition1() {
-            //    Add condition for Broyden 1-rank method convergence
-            return;
-        }
-    }
-
-private:
-    torch::Tensor x;
-    torch::Tensor y;
-    torch::Tensor p;
-    torch::Tensor func;
-    float eps1 = 1e-3;
-    float eps2 = 1e-3;
-    float eps3 = 1e-3;
-    float eps4 = 1e-3;
-    float lm_up = 11;
-    float lm_down = 9;
-    float lambda_lm = 10;
-    float sigma = 4.0;
+ private:
+  torch::Tensor x;
+  torch::Tensor y;
+  torch::Tensor p;
+  torch::Tensor func;
+  // float eps1 = 1e-3;
+  // float eps2 = 1e-3;
+  // float eps3 = 1e-3;
+  // float eps4 = 1e-3;
+  // float lm_up = 11;
+  // float lm_down = 9;
+  // float lambda_lm = 10;
+  // float sigma = 4.0;
 };
 
-#endif //SABR_TORCH_LM_H
+#endif // SABR_TORCH_LM_H
--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,5 @@
 #include <torch/torch.h>
+#include <fstream>
 #include <iostream>
 #include <list>
 #include <vector>
@@ -6,84 +7,109 @@
 #include "LevenbergMarquad.hpp"
 #include "sigma_SABR.hpp"
 
+// template <class Data>
+// auto data_points(Data data) {
+//   auto middle{[data](auto func) {
+//     return [data, func](auto... args) { return func(data, args...); };
+//   }};
 
-template <class Data>
-auto data_points(Data data) {
-  auto middle{[data](auto func){
-    return [data, func](auto... args) { return func(data, args...); };
-  }};
-
-  return middle;
-}
+//   return middle;
+// }
 
 static torch::Tensor f(torch::Tensor t, torch::Tensor p) {
-    return p[0] * exp(-t / p[1]) + p[2] * t * exp(-t / p[3]);
+  return p[0] * exp(-t / p[1]) + p[2] * t * exp(-t / p[3]);
 }
 
-static auto K = torch::linspace(0.04, 0.11, 25);
-        float S = 0.06,
-        T = 0.5,
-        alpha = 0.037561,
-        beta = 0.5,
-        rho = 0.100044,
-        nu = 0.573296;
+struct LM_PARAMS {
+  torch::Tensor x;
+  torch::Tensor init_p;
+  torch::Tensor y;
+  torch::Tensor func;
+  float sigma = 4.0; // std of datapoint
+  int lambda_lm = 10; // starting lambda
+  float eps1 = 1e-3;
+  float eps2 = 1e-3;
+  float eps3 = 1e-3;
+  float eps4 = 1e-3;
+  int lm_up = 11; // up coefficient
+  int lm_down = 9; // down coefficient
+};
 
 int main() {
-    torch::Tensor true_p = torch::randn({20, 10, 1, 50}); // True parameteres
-    torch::Tensor x_true = torch::linspace(0, 100, 25); // span of free parameter
-    torch::Tensor y_true = f(x_true, true_p); // fitted function observed values
+  std::fstream strm;
+  strm.open("input.txt", std::ios_base::out);
 
-    torch::Tensor init_p = true_p + pow((torch::randn(4) * 2), 2) + 4;
-/// // initial guess for parametes = true + noise
-    auto modified_f = data_points(x_true)(f);  // wrapped function (dependent on only parameters)
-    std::cout << "Initial guess for optimizer " << init_p << std::endl;
+  if (strm.is_open()) {
+    std::string str;
+    strm << str;
+    strm.close();
+  } else
+    std::cerr << "File could not be opened!" << std::endl;
 
-// params = { 'x' : x_true,
-//            'init_p': init_p,
-//            'y' : y_true,
-//            'func' : modified_f,  # model to be fitted
-//            'sigma' : 4.0,    # std of datapoint
-//            'lambda_lm' : 10, # starting lambda
-//            'eps1' : 1e-3,    
-//            'eps2' : 1e-3,
-//            'eps3' : 1e-3,
-//            'eps4' : 1e-3,
-//            'lm_up' : 11,     # up coefficient
-//            'lm_down' : 9     # down coefficient
-//            }
+  // TODO Input from file (matrix)
+  // striike,val,sigm
+  
+  torch::Tensor true_p = torch::randn({20, 10, 1, 50}); // True parameteres
+  torch::Tensor x_true = torch::linspace(0, 100, 25); // span of free parameter
+  torch::Tensor y_true = f(x_true, true_p); // fitted function observed values
 
-//     LevenbergMarquad lm(params)     
+  torch::Tensor init_p = true_p + pow((torch::randn(4) * 2), 2) + 4;
+  /// // initial guess for parametes = true + noise
+  auto modified_f =
+      data_points(x_true)(f); // wrapped function (dependent on only parameters)
+  std::cout << "Initial guess for optimizer " << init_p << std::endl;
 
-    LevenbergMarquad lm(init_p,x_true, y_true, modified_f,4.0,10,1e-3,1e-3,1e-3,1e-3);
-    for (int i = 0; i <= 1000; ++i)
-        lm.step();
+  //     LevenbergMarquad lm(params)
+  torch::Tensor f;
+  LM_PARAMS lm_params{};
+  LevenbergMarquad lm(
+      init_p,
+      x_true,
+      y_true,
+      modified_f,
+      lm_params.sigma,
+      lm_params.lambda_lm,
+      lm_params.eps1,
+      lm_params.eps2,
+      lm_params.eps3,
+      lm_params.eps4);
 
-    std::list<float> p_hat;
-    std::list<float> p;
-    std::list<float> lmbd;
+  for (int i = 0; i <= 1000; ++i)
+    lm.step();
 
-    for (int i = 0; i < 10; i++) {
-        p.push_back(float(torch::linalg::norm(lm.step(),2)));
-        p_hat.push_back(float(torch::linalg::norm(lm.p - true_p, auto opt_ord = 2)));
-        lmbd.push_back(lm.lambda_lm);
-    }
+  std::list<float> p_hat;
+  std::list<float> p;
+  std::list<float> lmbd;
 
-//TODO: Sigma
-    torch::Tensor K = torch::linspace(0.04, 0.11, 25);
-    float S = 0.06;
-    float T = 0.5;
-    float alpha = 0.037561;
-    float beta = 0.5;
-    float rho = 0.100044;
-    float nu = 0.573296;
+  for (int i = 0; i < 10; i++) {
+    p.push_back(float(torch::linalg::norm(lm.step(), 2)));
+    p_hat.push_back(
+        float(torch::linalg::norm(lm.p - true_p, auto opt_ord = 2)));
+    lmbd.push_back(lm.lambda_lm);
+  }
 
-    init_p = torch::randn({0.1*3});
-    true_p = torch::randn([alpha, nu, rho]);
+  // TODO: Sigma
+  torch::Tensor K = torch::linspace(0.04, 0.11, 25);
+  float S = 0.06;
+  float T = 0.5;
+  float alpha = 0.037561;
+  float beta = 0.5;
+  float rho = 0.100044;
+  float nu = 0.573296;
+
+  init_p = torch::randn({0.1 * 3});
+  true_p = torch::randn([ alpha, nu, rho ]);
     torch::Tensor(y_mkt = sigma_SABR(K, true_p);
 
+
+// TODO
     auto modified_sabr = data_points(K),(sigma_SABR);
+// TODO
 
-
+    struct SIGMA_PARAMS : LM_PARAMS {torch::Tensor x = K,modified_sabr([ alpha, nu, rho]), func = modified_sabr, 
+    sigma = 1.0,lambda_lm = 0.1, lm_up = 10, lm_down = 9};
+   
+    // TODO
     LevenbergMarquad lm_sigma(x_true, init_p, y_true, modified_f);
     for (int i = 0; i <= 1000; ++i)
         lm.step();
